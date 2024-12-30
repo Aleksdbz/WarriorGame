755444f9cf6e5506c6764c63c810ea78ddf83a7f
Aleksandar Ivic <Aleksandarivic927@gmail.com>
2024-12-30 20:18:37 +0100

    Debug helper

--- Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
+++ Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
@@ -2,4 +2,12 @@
 
 
 #include "Characters/WarriorHeroCharacter.h"
+#include "WarriorDebugHelper.h"
 
+
+void AWarriorHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DeBug::Print(TEXT("Working"));
+}
